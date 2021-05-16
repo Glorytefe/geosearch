7df4d4fa2fb681c94be049bb898c4b5864ddc9fd
Glorytefe <glorytefe@gmail.com>
2021-05-16 20:46:32 +0100

    chore: changed from function class

--- script/index.js
+++ script/index.js
@@ -1,46 +1,55 @@
-// variables
-let searchInp = document.getElementById('se');
-let searchWord = document.getElementById('searchs')
-let searchHold = document.getElementById('hold');
-let vis = document.getElementById('hide');
-// arrs
-const arr1 = ['germany', 'italy', 'france', 'gambia', 'germanya'];
 
+class SEARCH {
+
+    constructor(){
+        // this.searchInp = document.getElementById('se');
+        this.searchHold = document.getElementById('hold');
+        this.vis = document.getElementById('hide');
+        this.arr1 = ['germany', 'italy', 'france', 'gambia', 'germanya'];
+    }
+    
 // get search value function
-const getWord = (e) =>{
+    getword(e){
         e.preventDefault();
-       let searchStr= e.target.value.toLowerCase();
-        // call filtersearch
-        filterSearch(searchStr);
-}
-// getword ends
-// invoke funct getWord on form submission
-searchWord.addEventListener('input', getWord);
-// filter for location
- const filterSearch = (searchStr) =>  {
-    //  arr to hold search result
+        let searchStr= e.target.value.toLowerCase();
+         // call filtersearch
+         this.filterSearch(searchStr);
+    }
+// filter search
+    filterSearch (searchStr){
+        //  arr to hold search result
     const searchRes = []
     if(searchStr !== ''){
-        arr1.forEach((val) =>{
+        this.arr1.forEach((val) =>{
         if(val.toLowerCase().includes(searchStr)){
             searchRes.push(val);
         }
     });
 }
 // call display search result funct
-      dispSearch(searchRes) 
+      this.dispSearch(searchRes) 
     }
-// filter ends
 
-// display search
-const dispSearch = (searchRes)=>{
-    if(searchRes){
-    searchHold.innerHTML = '';
-        vis.style.visibility = 'visible';
-       searchRes.forEach((res)=>{
-         let p= `<p class="hovera"><i class="fas fa-map-marker-alt px2 colblue"></i>${res}</p>`
-            searchHold.innerHTML += p; 
-        })
+    // display search 
+    dispSearch(searchRes){
+        if(searchRes){
+            this.searchHold.innerHTML = '';
+                this.vis.style.visibility = 'visible';
+                searchRes.forEach((res)=>{
+                 let p= `<p class="hovera"><i class="fas fa-map-marker-alt px2 colblue"></i>${res}</p>`
+                    this.searchHold.innerHTML += p; 
+                })
+            }
     }
 }
-    
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    // variable
+    let searchWord = document.getElementById('searchs');
+const searchFunct = new SEARCH();
+    // invoke funct getWord on form submission
+    searchWord.addEventListener('input', (e)=>{
+        searchFunct.getword(e)
+    });
+
+} )
